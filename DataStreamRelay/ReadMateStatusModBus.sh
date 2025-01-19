3c42c2507de4adf885a923b61a334ba4be3caa6e
Dan Popescu <35501848+chinezbrun@users.noreply.github.com>
2025-01-19 18:51:59 +0200

    Delete ReadMateStatusModBus.sh

--- DataStreamRelay/ReadMateStatusModBus.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# task from local PI
-#/usr/bin/python3 /var/www/html/MonitorMate_mod/DataStreamRelay/ReadMateStatusModBus.py
-# task from NAS 
-/volume1/@appstore/python3/bin/python3 /volume1/web/MonitorMate_mod/DataStreamRelay/ReadMateStatusModBus.py
